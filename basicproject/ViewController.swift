e04cab53151e0c14196afce7b259906b99c17d11
iOS_dev <ios_dev@iOS-devdeMac-mini.local>
2022-06-07 19:03:02 +0800

    [First Push]

---
+++ basicproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basicproject
+//
+//  Created by iOS_dev on 2022/6/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
